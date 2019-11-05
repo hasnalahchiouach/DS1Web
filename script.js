42e30e8eaf53c67cf144d723235b3fa65a153381
Client18@EST.LAN <Client18@EST.LAN>
2019-11-05 15:10:39 +0000

    Q2

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ var init = function()
 }
 
 document.body.onload = init;
+
+
+///////////////////////////////
+var btn_lancer = document.querySelector(".btn-lancer");
+var images = ["de-1.png","de-2.png","de-3.png","de-4.png","de-5.png","de-6.png"];
+var tempScore = 0;
+
+function lance(){
+	
+	var joueur_actif = document.querySelector(".actif .score-joueur-courent");
+	var nbr_alt = Math.floor(Math.random()*6);
+	var alt = images[nbr_alt];
+
+	de.setAttribute("src", alt);
+	de.style.visibility = "visible";
+	nbr_alt++;
+	tempScore += nbr_alt;
+
+
+	if(nbr_alt!=1)
+	joueur_actif.innerHTML = tempScore;
+
+
+	else {
+	tempScore = 0;
+	joueur1.classList.toggle('actif');
+	joueur0.classList.toggle('actif');
+	joueur_actif.innerHTML = 0;
+	de.style.visibility = "hidden";
+	}
+}
+btn_lancer.addEventListener("click",lance,false);
+
+/////////////////////////////////
